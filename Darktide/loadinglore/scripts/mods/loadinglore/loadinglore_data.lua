d0fc6093fe69dc5681dc0e5687dbce0bf34977eb
xsSplater <xssplater@gmail.com>
2023-04-06 18:46:03 +0300

    Added the possibility of localization of the name.

--- Darktide/loadinglore/scripts/mods/loadinglore/loadinglore_data.lua
+++ Darktide/loadinglore/scripts/mods/loadinglore/loadinglore_data.lua
@@ -1,7 +1,7 @@
 local mod = get_mod("loadinglore")
 
 return {
-	name = "loadinglore",
+	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
 	is_togglable = false,
 	options = {
